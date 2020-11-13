94d91655854e383bea8af87591f289d66bb36c87
ElvisSethi <74413955+ElvisSethi@users.noreply.github.com>
2020-11-13 23:38:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Elvis Sethi',                   # Type in your name
   author_email = 'sethi21elvis@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/ElvisSethi',   # Provide either the link to your github or to your website
-  download_url = '',    # I explain this later on
+  download_url = 'https://github.com/ElvisSethi/TOPSIS-Elvis-101803397/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['TOPSIS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
